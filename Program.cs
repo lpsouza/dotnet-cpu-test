5e0a016e109653245f864a665d560da1bb6fbac7
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2018-05-23 18:18:45 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace CPUTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int percentage = 100;
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+            {
+                (new Thread(() =>
+                {
+                    Stopwatch watch = new Stopwatch();
+                    watch.Start();
+                    while (true)
+                    {
+            if (watch.ElapsedMilliseconds > percentage)
+                        {
+                            Thread.Sleep(100 - percentage);
+                            watch.Reset();
+                            watch.Start();
+                        }
+                    }
+                })).Start();
+            }
+        }
+    }
+}
